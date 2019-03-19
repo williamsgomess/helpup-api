ba9b421ce1524120ae1aa151b1ddaa526505d3e1
Williams Gomes <williams.gomes@a.ficr.edu.br>
2019-03-19 16:27:18 -0300

    Põe porta dinâmica

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -1,7 +1,7 @@
 import bodyParser from 'body-parser'
 
 module.exports = app => {
-    app.set('port', 3000);
+    app.set('port', process.env.PORT || '3000');
     app.set('json spaces', 4);
     app.use(bodyParser.json());
     app.use((req, res, next) => {
