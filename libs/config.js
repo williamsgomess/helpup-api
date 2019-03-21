606f5b85ca663bf71bf9102bb720be316bf8627f
Williams Gomes <williams.gomes@a.ficr.edu.br>
2019-03-21 15:30:49 -0300

    Resolver problema no Access-Control-Allow-Origin

--- libs/config.js
+++ libs/config.js
@@ -2,18 +2,18 @@ import Sequelize from 'sequelize';
 
 const Op = Sequelize.Op;
 module.exports = {
-    database: 'heroku_49ac9aecbcc66ae',
-    username: 'b581dc31107300',
-    password: '0ac80553',
-    params: {
-        dialect: 'mysql',
-        host: 'us-cdbr-iron-east-03.cleardb.net',
-        //port: 3306,
-        pool: {
-            max: 5,
-            min: 0,
-            idle: 10000
-        },
-        operatorAliases: Op,
-    }
+  database: 'heroku_49ac9aecbcc66ae',
+  username: 'b581dc31107300',
+  password: '0ac80553',
+  params: {
+    dialect: 'mysql',
+    host: 'us-cdbr-iron-east-03.cleardb.net',
+    //port: 3306,
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000
+    },
+    operatorAliases: Op,
+  }
 };
--- libs/middlewares.js
+++ libs/middlewares.js
@@ -1,11 +1,13 @@
 import bodyParser from 'body-parser'
+import cors from 'cors'
 
 module.exports = app => {
-    app.set('port', process.env.PORT || '3000');
-    app.set('json spaces', 4);
-    app.use(bodyParser.json());
-    app.use((req, res, next) => {
-        delete req.body.id;
-        next();
-    });
+  app.set('port', process.env.PORT || '3000');
+  app.set('json spaces', 4);
+  app.use(bodyParser.json());
+  app.use((req, res, next) => {
+    delete req.body.id;
+    next();
+  });
+  app.use(cors({ origin: '*' }));
 }
